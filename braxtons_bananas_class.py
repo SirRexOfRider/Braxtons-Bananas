fd01406619dd06b5c98b996a9f314620e1650254
Braxton Rider <rider.braxton@gmail.com>
2023-10-05 11:10:38 -0600

    Update braxtons_bananas_class.py

--- braxtons_bananas_class.py
+++ braxtons_bananas_class.py
@@ -6,7 +6,20 @@ POB = 10.50
 #Make class
 class BananaBungaloo:
     def __init__(self):
-        pass
+        self._bananas = 0
+        self.menu_items = ["Banana Bread $", "Banana smoothie $", "Banana $", "Gormet Banana Sauce $"]
+        self.menu_price = [5.00,3.50,10,10]
+
+        self.cart = []
+
+
+    @property
+    def name(self):
+        return self._bananas
+
+    @name.setter
+    def name(self,bananas):
+        self._banana = bananas
 
     def get_number_of_bananas(self):
         if self._bananas > 0:
@@ -20,6 +33,7 @@ class BananaBungaloo:
 
     #TODO:Calculate price
     def calc(self, bananas):   
+        
         self._bananas = bananas
         self._price = POB * bananas
         
